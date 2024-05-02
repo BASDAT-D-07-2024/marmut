05b2154839bd66ee61aa2aefcb79ef42f089c250
riapis <fari.hafizh@ui.ac.id>
2024-05-02 19:23:22 +0700

    kelola podcast

--- podcast/views.py
+++ podcast/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 def play_podcast(request):
 
     return render(request, "play_podcast.html")
+
+def create_podcast(request):
+
+    return render(request, "create_podcast.html")
+
+def create_ep_podcast(request):
+
+    return render(request, "create_ep_podcast.html")
+
+def list_podcast(request):
+
+    return render(request, "list_podcast.html")
+
+def list_ep_podcast(request):
+
+    return render(request, "list_ep_podcast.html")
