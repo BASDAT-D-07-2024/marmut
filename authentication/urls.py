a4a982b71cb322b5c89ce5cd0c9341d8e59583b1
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-07 14:46:54 +0700

    feat: integrate login and logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,7 @@ app_name = 'authentication'
 urlpatterns = [
     path('', login_register, name='login_register'),
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
     path('register/', register, name='register'),
     path('register/user/', register_user, name='register_user'),
     path('register/label/', register_label, name='register_label'),
--- authentication/views.py
+++ authentication/views.py
@@ -1,17 +1,69 @@
-from django.shortcuts import render
+import psycopg2
+from utils.db_utils import get_db_connection
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@require_http_methods(['GET'])
 def login_register(request):
+    if request.session.get('username') is not None:
+        return redirect('dashboard')
     return render(request, 'login-register.html')
 
+@require_http_methods(['GET', 'POST'])
 def login(request):
-    return render(request, 'login.html')
+    if request.session.get('username') is not None:
+        return redirect('dashboard')
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
 
+        try:
+            connection = get_db_connection()
+
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM akun WHERE email = %s AND password = %s", (email, password))
+            user = cursor.fetchone()
+
+            if user is not None:
+                request.session['username'] = user[2]
+                return redirect('main:dashboard')
+            else:
+                messages.error(request, 'Email or password is incorrect')
+                return redirect('authentication:login')
+
+        except psycopg2.Error as e:
+            print(e)
+            return HttpResponse("Error occurred while connecting to the database")
+
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
+
+    else:
+        return render(request, 'login.html')
+
+@require_http_methods(['GET'])
 def register(request):
+    if request.session.get('username') is not None:
+        return redirect('dashboard')
     return render(request, 'register.html')
 
+@require_http_methods(['GET', 'POST'])
 def register_user(request):
+    if request.session.get('username') is not None:
+        return redirect('dashboard')
     return render(request, 'register-user.html')
 
+@require_http_methods(['GET', 'POST'])
 def register_label(request):
+    if request.session.get('username') is not None:
+        return redirect('dashboard')
     return render(request, 'register-label.html')
+
+@require_http_methods(['GET'])
+def logout(request):
+    request.session.flush()
+    return redirect('authentication:login_register')
