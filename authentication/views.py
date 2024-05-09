e7e5f1f93fe2d47972b11fe18f55477f6a76e31e
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-10 01:24:03 +0700

    feat: Add login functionality with role-based access

--- authentication/views.py
+++ authentication/views.py
@@ -18,16 +18,34 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
-
+        
         try:
             connection = get_db_connection()
-
+            
             cursor = connection.cursor()
-            cursor.execute("SELECT * FROM akun WHERE email = %s AND password = %s", (email, password))
+            if is_label(email):
+                cursor.execute("SELECT * FROM label WHERE email = %s AND password = %s", (email, password))
+            else:
+                cursor.execute("SELECT * FROM akun WHERE email = %s AND password = %s", (email, password))
             user = cursor.fetchone()
 
             if user is not None:
-                request.session['username'] = user[2]
+                request.session['role'] = []
+                if is_label(email):
+                    request.session['username'] = user[1]
+                    request.session['role'] += ['label']
+                else:
+                    request.session['username'] = user[2]
+                    if is_premium(email):
+                        request.session['role'] += ['premium']
+                    else:
+                        request.session['role'] += ['nonpremium']
+                    if is_podcaster(email):
+                        request.session['role'] += ['podcaster']
+                    if is_artist(email):
+                        request.session['role'] += ['artist']
+                    if is_songwriter(email):
+                        request.session['role'] += ['songwriter']
                 return redirect('main:dashboard')
             else:
                 messages.error(request, 'Email or password is incorrect')
@@ -67,3 +85,113 @@ def register_label(request):
 def logout(request):
     request.session.flush()
     return redirect('authentication:login_register')
+
+def is_premium(email):
+    try:
+        connection = get_db_connection()
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM premium WHERE email = %s", (email,))
+        user = cursor.fetchone()
+
+        if user is not None:
+            return True
+        else:
+            return False
+
+    except psycopg2.Error as e:
+        print(e)
+        return HttpResponse("Error occurred while connecting to the database")
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+def is_podcaster(email):
+    try:
+        connection = get_db_connection()
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM podcaster WHERE email = %s", (email,))
+        user = cursor.fetchone()
+
+        if user is not None:
+            return True
+        else:
+            return False
+
+    except psycopg2.Error as e:
+        print(e)
+        return HttpResponse("Error occurred while connecting to the database")
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+def is_artist(email):
+    try:
+        connection = get_db_connection()
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM artist WHERE email_akun = %s", (email,))
+        user = cursor.fetchone()
+
+        if user is not None:
+            return True
+        else:
+            return False
+
+    except psycopg2.Error as e:
+        print(e)
+        return HttpResponse("Error occurred while connecting to the database")
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+def is_songwriter(email):
+    try:
+        connection = get_db_connection()
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM songwriter WHERE email_akun = %s", (email,))
+        user = cursor.fetchone()
+
+        if user is not None:
+            return True
+        else:
+            return False
+
+    except psycopg2.Error as e:
+        print(e)
+        return HttpResponse("Error occurred while connecting to the database")
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+def is_label(email):
+    try:
+        connection = get_db_connection()
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM label WHERE email = %s", (email,))
+        label = cursor.fetchone()
+
+        if label is not None:
+            return True
+        else:
+            return False
+
+    except psycopg2.Error as e:
+        print(e)
+        return HttpResponse("Error occurred while connecting to the database")
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
