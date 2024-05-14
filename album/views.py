cdecc6172bbab318dd964a27b64005a8104d6be6
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-14 13:36:18 +0700

    feat: add view album functionality

--- album/views.py
+++ album/views.py
@@ -56,7 +56,6 @@ def list_album(request):
 
                 return render(request, 'list-album.html', context)
         except psycopg2.Error as error:
-            print("masuk error: ", error)
             messages.error(request, 'Error while fetching data from database')
             return render(request, 'list-album.html')
         finally:
@@ -66,7 +65,50 @@ def list_album(request):
     return redirect('main:dashboard')
 
 def view_album(request, id):
+    if request.session.get('role') is None:
+        return redirect('authentication:login')
+    
+    role = request.session.get('role')
+
     context = {
         'id': id,
+        'songs': [],
     }
-    return render(request, 'view-album.html', context)
+
+    if 'label' in role or 'artist' in role or 'songwriter' in role:
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor()
+
+            cursor.execute('SELECT * FROM ALBUM WHERE id = %s', (id,))
+            album = cursor.fetchone()
+            context['judul'] = album[1]
+
+            cursor.execute('SELECT * FROM SONG WHERE id_album = %s', (id,))
+            songs = cursor.fetchall()
+
+            for i in range(len(songs)):
+                cursor.execute('SELECT judul, durasi FROM KONTEN WHERE id = %s', (songs[i][0],))
+                konten = cursor.fetchone()
+
+                cursor.execute('SELECT count(*) FROM DOWNLOADED_SONG WHERE id_song = %s', (songs[i][0],))
+                total_download = cursor.fetchone()
+
+                song = {
+                    'id': songs[i][0],
+                    'judul': konten[0],
+                    'durasi': konten[1],
+                    'total_play': songs[i][3],
+                    'total_download': total_download[0],
+                }
+                context['songs'].append(song)
+
+            return render(request, 'view-album.html', context)
+        except psycopg2.Error as error:
+            messages.error(request, 'Error while fetching data from database')
+            return render(request, 'view-album.html')
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
+    return redirect('main:dashboard')
