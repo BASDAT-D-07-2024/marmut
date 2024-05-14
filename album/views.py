3e2f12449980a053277ce8b6e4c4258018396aa4
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-14 12:04:01 +0700

    feat: add list album functionality

--- album/views.py
+++ album/views.py
@@ -1,7 +1,69 @@
-from django.shortcuts import render
+import psycopg2
+from django.contrib import messages
+from utils.db_utils import get_db_connection
+from django.shortcuts import render, redirect
 
 def list_album(request):
-    return render(request, 'list-album.html')
+    if request.session.get('role') is None:
+        return redirect('authentication:login')
+    
+    role = request.session.get('role')
+    user = request.session.get('user')
+
+    context = {
+        'albums': [],
+        'is_label': 'label' in role,
+        'is_artist': 'artist' in role,
+        'is_songwriter': 'songwriter' in role,
+    }
+
+    if 'label' in role or 'artist' in role or 'songwriter' in role:
+        try:
+            connection = get_db_connection()      
+            cursor = connection.cursor()
+            if 'label' in role:
+                cursor.execute('SELECT * FROM LABEL WHERE email = %s', (user['email'],))
+                label = cursor.fetchone()
+
+                cursor.execute('SELECT * FROM ALBUM WHERE id_label = %s', (label[0],))
+                albums = cursor.fetchall()
+
+                for i in range(len(albums)):
+                    album = {
+                        'id': albums[i][0],
+                        'judul': albums[i][1],
+                        'jumlah_lagu': albums[i][2],
+                        'total_durasi': albums[i][4],
+                    }
+                    context['albums'].append(album)
+
+                return render(request, 'list-album.html', context)
+            elif 'artist' in role or 'songwriter' in role:
+                cursor.execute('SELECT * FROM ALBUM')
+                albums = cursor.fetchall()
+
+                for i in range(len(albums)):
+                    cursor.execute('SELECT nama FROM LABEL WHERE id = %s', (albums[i][3],))
+                    label = cursor.fetchone()
+                    album = {
+                        'id': albums[i][0],
+                        'judul': albums[i][1],
+                        'label': label[0],
+                        'jumlah_lagu': albums[i][2],
+                        'total_durasi': albums[i][4],
+                    }
+                    context['albums'].append(album)
+
+                return render(request, 'list-album.html', context)
+        except psycopg2.Error as error:
+            print("masuk error: ", error)
+            messages.error(request, 'Error while fetching data from database')
+            return render(request, 'list-album.html')
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
+    return redirect('main:dashboard')
 
 def view_album(request, id):
     context = {
