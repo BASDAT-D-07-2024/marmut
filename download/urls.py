2951f827b351278e54d4d1548c89d001d30d5c1d
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-19 22:07:55 +0700

    feat: rd downloaded song

--- download/urls.py
+++ download/urls.py
@@ -4,5 +4,9 @@ from download.views import *
 app_name = 'download'
 
 urlpatterns = [
-    path('', list_download, name='list_album'),
+    path('', list_download, name='list_download'),
+    path('delete/<uuid:id_song>', delete_download, name='delete_download'),
+
+    path('/', list_download, name='list_download'),
+    path('/delete/<uuid:id_song>/', delete_download, name='delete_download'),
 ]
