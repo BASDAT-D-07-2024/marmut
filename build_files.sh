9b3d1813c19e7cae019b2e3d35983d4f3c827644
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-19 15:24:05 +0700

    chore: deploy back to vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,12 @@
-# build_files.sh
-python3.9 -m pip install -r requirements.txt
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+PYTHON_PATH=/usr/bin/python3.9
+
+# Install dependencies
+$PYTHON_PATH -m pip install --upgrade setuptools
+$PYTHON_PATH -m pip install -r requirements.txt
+
+# Jalankan migrasi dan kumpulkan static files
+$PYTHON_PATH manage.py migrate 
+$PYTHON_PATH manage.py collectstatic
