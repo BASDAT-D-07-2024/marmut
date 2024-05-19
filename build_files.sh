115b13df2c079a3e61f2c483de053f8df9f9c4b2
Robert Benyamin <108232315+RobertBenyamin@users.noreply.github.com>
2024-05-19 14:44:40 +0700

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install python3.9-pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
 
 # build_files.sh
 python3.9 -m pip install -r requirements.txt
